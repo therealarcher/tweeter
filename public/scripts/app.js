3dafc0c79b2713ebda0bd8cb2a5c490e23c03f7c
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-07-18 21:26:08 +0000

    have a site that is not reloading, and has alerts sent to user

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,6 +5,52 @@
  */
 
 
+// Test / driver code (temporary)
+// 
+$(document).ready(function() {
+  addSubmitHandler();
+  loadTweets();
+});
+
+const addSubmitHandler = function() {
+  $(".new-tweet form").on('submit', function (event) {
+    event.preventDefault();
+    if ($('#tweet-string').val() === "" || $('#tweet-string').val() === null) {
+      alert("Please enter a value");
+      return;
+    } else if ($('#tweet-string').val().length > 140) {
+      alert("Maximum character length is 140, please reduce and resubmit");
+      return;
+    } else {
+      console.log('performing ajax call...');      
+      $.ajax('/tweets', {method: 'POST', data: $(this).serialize()})
+      .done(function (response) {
+        console.log('Success: ', response);
+        loadTweets();
+        $('#tweet-string').val("");
+        $(".counter").text(140);
+      })
+      .fail(function (error) {
+        console.error(error);
+      });
+    }
+  });
+};
+
+const loadTweets = function () {
+  // const $button = $('#submit-button');
+  // $button.on('click', function () {
+    // console.log('Button clicked, performing ajax call...');
+    
+    $.ajax('/tweets', { method: 'GET', dataType: 'json' })
+    //$.getJSON('/tweets');
+    .done(function (response) {
+      console.log('Success with Get: ', response);
+      renderTweets(response);
+    });
+  // });
+};
+
 const createTweetElement = function (obj) {
   // const $tweet = $("<article>").addClass("content"); //check
   //console.log($tweet);
@@ -19,7 +65,7 @@ const createTweetElement = function (obj) {
         <div class="tweet-text"> ${obj.content.text} </div>
     </p>
     <footer>
-      <span class="days-ago">${Date()} </span>
+      <span class="days-ago">${Date(obj.created_at)} </span>
     </footer>
   
   </article>
@@ -31,40 +77,37 @@ const createTweetElement = function (obj) {
 
 const renderTweets = function (tweets) {
   const container = $('#tweet-container');
+  container.empty();
   for (let post of tweets) {
+    //console.log(post)
     const rendered = createTweetElement(post);
-    container.append(rendered)
+    container.prepend(rendered)
   }
 }
 
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-// const $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-// 
-$(document).ready(function() {
-  renderTweets(tweetData);
-});
+// const tweetData = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
+// const $tweet = createTweetElement(tweetData);
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,3 @@
-//console.log('testing');
 
 $(document).ready(function() {
   console.log('ready')
